e78ab7ff32e9440d5fe16f24dd9a0d3ea51fae53
huyldph <ledinhhuy26032004@gmail.com>
2025-10-16 10:16:57 +0700

    clean up file

--- app/member/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function MemberHomePage() {
-    return (
-        <div className="p-6">
-            <h1 className="text-xl font-semibold">Khu vực bạn đọc</h1>
-            <p className="text-muted-foreground">Chào mừng bạn đến trang dành cho bạn đọc.</p>
-        </div>
-    );
-}
